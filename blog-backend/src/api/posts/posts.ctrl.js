43df90d329e295f316b434a875044c212b194f85
paekma09@gmail.com <paekma09@gmail.com>
2024-02-07 14:51:40 +0900

    chapter22-3_Mongoose_Install & Setting_Done

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -1,4 +1,4 @@
-let posiId = 1; //id 의 초기값입니다.
+let postId = 1; //id 의 초기값입니다.
 
 //posts 배열 초기 데이터
 const posts = [
@@ -17,8 +17,8 @@ const posts = [
 exports.write = ctx => {
   //REST API 의 Request Body 는 ctx.request.body 에서 조회할 수 있습니다.
   const { title, body } = ctx.request.body;
-  posiId += 1;  //기존 postId 값에 1을 더합니다.
-  const post = { id: posiId, title, body };
+  postId += 1;  //기존 postId 값에 1을 더합니다.
+  const post = { id: postId, title, body };
   posts.push(post);
   ctx.body = post;
 };
