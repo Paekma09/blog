87a4ce4f49d64bd5c83f66e3edd09fd253ba6d70
KJ Nam <paekma09@gmail.com>
2024-02-10 16:07:20 +0900

    chapter24_2_Register&Login_Done

--- blog-frontend/src/components/auth/AuthForm.js
+++ blog-frontend/src/components/auth/AuthForm.js
@@ -58,12 +58,23 @@ const ButtonWithMarginTop = styled(Button)`
     margin-top: 1rem;
 `;
 
+/*
+* 에러를 보여 줍니다.
+* */
+const ErrorMessage = styled.div`
+    color: red;
+    text-align: center;
+    font-size: 0.875rem;
+    margin-top: 1rem;
+`;
+
+
 const textMap = {
   login: '로그인',
   register: '회원가입'
 };
 
-const AuthForm = ({ type, form, onChange, onSubmit }) => {
+const AuthForm = ({ type, form, onChange, onSubmit, error }) => {
   const text = textMap[type];
   return (
     <AuthFormBlock>
@@ -74,6 +85,7 @@ const AuthForm = ({ type, form, onChange, onSubmit }) => {
         {type === 'register' && (
           <StyledInput autoComplete="new-password" name="passwordConfirm" placeholder="비밀번호 확인" type="password" onChange={onChange} value={form.passwordConfirm} />
         )}
+        {error && <ErrorMessage>{error}</ErrorMessage>}
         <ButtonWithMarginTop cyan fullWidth style={{ marginTop: '1rem' }}>
           {text}
         </ButtonWithMarginTop>
